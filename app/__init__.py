83f9ecb71b0bb2541c6675e92392db47cd0834b6
Natã R <natarabelo0@gmail.com>
2025-03-28 18:17:04 -0300

    Ponto de Controle 1

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'auth.login'
     bcrypt.init_app(app)
 
     from app.routes.auth import auth
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,18 +1,27 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from app.models import Usuario
 from app import bcrypt, db
 
 auth = Blueprint('auth', __name__)
 
+
+@auth.route('/deletar/<int:id>', methods=['GET', 'POST'])
+def deletar_usuario(id):
+    usuario = Usuario.query.get_or_404(id)
+    db.session.delete(usuario)
+    db.session.commit()
+    flash('Usuario excluido com sucesso', 'success')
+    return redirect(url_for('auth.login'))
+
 @auth.route('/registrar', methods=['GET', 'POST'])
-@login_required
 def registrar():
     if request.method == 'POST':
         email           = request.form.get('email')
         nome            = request.form.get('nome')
         senha           = request.form.get('senha')
-        tipo_usuario    = request.form.get('tipo_usuario')
+        tipo_usuario    = 'cliente'
         empresa         = request.form.get('empresa')
 
         usuario = Usuario.query.filter_by(email=email).first()
@@ -20,7 +29,7 @@ def registrar():
         if usuario:
             flash('Email já cadastrado.', 'danger')
             return redirect(url_for('auth.login'))
-        
+                
         usuario = Usuario(
             nome=nome, 
             email=email,
@@ -40,12 +49,16 @@ def login():
         email = request.form.get('email')
         senha = request.form.get('senha')
 
+        print(f"Email recebido: {email}")
+        print(f"Senha recebida: {senha}")
+        
         usuario = Usuario.query.filter_by(email=email).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, senha):
+        if usuario and usuario.senha and check_password_hash(usuario.senha, senha):
             login_user(usuario)
-            return redirect(url_for('main.index'))
+            return redirect(url_for('main.menu'))
         else:
-            flash('Email ou senha incorretos.', 'danger')
+            flash('E-mail ou senha inválidos.', 'error')
+            return redirect(url_for('auth.login'))
 
     return render_template('login.html')
 
